21af66530763cadd04cee2a29f5a42dd67c4c316
Luvneesh Kumar <luvneeshkumar@Luvneeshs-MacBook-Pro.local>
2019-12-13 21:14:09 +0530

    categorized

--- Frame_extract.py
+++ Frame_extract.py
@@ -1,13 +1,13 @@
 import cv2
 
-cap = cv2.VideoCapture("Data_vid/side.mp4")
+cap = cv2.VideoCapture("Data_vid/back.mp4")
 i=0
 while(cap.isOpened()):
     
     ret,img=cap.read()
     
-    filename='side'
-    foldername="image/"
+    filename='back'
+    foldername="image/"+filename+'/'
     img_filename=foldername+filename+'_'+str(i)+'.jpg'
     # print(img.shape)
     cv2.imwrite(img_filename,img)
